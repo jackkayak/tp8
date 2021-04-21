bf1dd7c9e97786b5b195a8944d85b289568e1b24
Jack Kay <jacktkay99@gmail.com>
2021-04-21 22:05:35 +0000

    PHP recipes

---
+++ recipes.php
@@ -0,0 +1,30 @@
+<?
+
+// PHP response code for tp8
+
+$recipes = array();
+
+$recipes["RaspberryStreuselTart"] = array();
+$recipes["RaspberryStreuselTart"]["ingredients"] = array();
+$recipes["RaspberryStreuselTart"]["equipment"] = array();
+$recipes["RaspberryStreuselTart"]["directions"] = array();
+
+$recipes["RaspberryStreuselTart"]["ingredients"][0] = "6 tablespoons unsalted butter";
+$recipes["RaspberryStreuselTart"]["ingredients"][1] = "1 ¼ cups all-purpose flour";
+$recipes["RaspberryStreuselTart"]["ingredients"][2] = "6 tablespoons white sugar";
+$recipes["RaspberryStreuselTart"]["ingredients"][3] = "¼ teaspoon ground cinnamon";
+$recipes["RaspberryStreuselTart"]["ingredients"][4] = "1/8 teaspoon salt";
+$recipes["RaspberryStreuselTart"]["ingredients"][5] = "Custard :";
+$recipes["RaspberryStreuselTart"]["ingredients"][6] = "1 ¾ cups light cream";
+$recipes["RaspberryStreuselTart"]["ingredients"][7] = "⅓ cup white sugar";
+$recipes["RaspberryStreuselTart"]["ingredients"][8] = "4 teaspoons cornstarch";
+$recipes["RaspberryStreuselTart"]["ingredients"][9] = "4 egg yolks";
+$recipes["RaspberryStreuselTart"]["ingredients"][10] = "1 tablespoon unsalted butter";
+$recipes["RaspberryStreuselTart"]["ingredients"][11] = "1 teaspoon vanilla extract";
+$recipes["RaspberryStreuselTart"]["ingredients"][12] = "1 tablespoon kirschwasser";
+$recipes["RaspberryStreuselTart"]["ingredients"][13] = "1 (11 inch) shortbread tart crust, baked";
+$recipes["RaspberryStreuselTart"]["ingredients"][14] = "⅔ cup apricot preserves";
+$recipes["RaspberryStreuselTart"]["ingredients"][15] = "2 tablespoons water";
+$recipes["RaspberryStreuselTart"]["ingredients"][16] = "2 ½ pints fresh raspberries";
+$recipes["RaspberryStreuselTart"]["ingredients"][17] = "2 ½ tablespoons sifted confectioners' sugar";
+
---
+++ scripts.js
@@ -0,0 +1,110 @@
+// generic AJAX function to load fromFile into object with ID whereTo
+function loadFileInto(fromFile, whereTo) {
+
+	// creating a new XMLHttpRequest object
+	ajax = new XMLHttpRequest();
+
+	// defines the GET/POST method, source, and async value of the AJAX object
+	ajax.open("GET", fromFile, true);
+
+	// prepares code to do something in response to the AJAX request
+	ajax.onreadystatechange = function() {
+		
+			if ((this.readyState == 4) && (this.status == 200)) {
+				document.getElementById(whereTo).innerHTML = this.responseText;
+				
+			} else if ((this.readyState == 4) && (this.status != 200)) {
+				console.log("Error: " + this.responseText);
+				
+			}
+		
+	} // end ajax.onreadystatechange
+
+	// now that everything is set, initiate request
+	ajax.send();
+
+}
+
+
+
+
+
+
+
+/* object constructor */
+
+function Recipe(recipeName, imageURL, contributorName, recipeIdentifier) {
+  this.name = recipeName;
+  this.imgsrc = imageURL; 
+  this.contributor = contributorName;
+  this.indentifier = ingredientFilename;
+  this.dirFile = directionsFilename;
+  this.equipFile = equipmentFilename;
+  
+  this.displayRecipe = function() {
+    document.querySelector("h1").innerHTML = this.name;
+    
+     document.querySelector("h3").innerHTML = "contributed by: " + this.contributor;
+   document.querySelector("#h1").style.backgroundImage = "url(" + this.imgsrc + ")";
+    
+    
+     loadFileInto(this.ingFile, "ingredients");
+      loadFileInto(this.equipFile, "equipment");
+      loadFileInto(this.dirFile, "directions");
+    
+  }
+  
+  
+  
+  
+  
+ 
+  
+  
+  
+}
+
+ RaspberryStreuselTart = new Recipe("Raspberry Streusel Tart", 
+                                   "https://source.unsplash.com/2nAR96Mil_c",
+                                   "Jack",
+                                   "RaspberryStreuselTart"
+                               
+                                    
+                                   );
+
+
+
+ /* change font color using click function on h1 to green */
+
+
+ function myFunction() {
+   document.getElementById("h1").style.color = "green";
+ }
+
+
+ /* dissapearing lists */
+a = document.querySelector("div#column1");
+a.onclick = function () {
+  a.classList.toggle("iClicked");
+};
+
+/* this way works, overcomplexed it the first time */
+
+b = document.querySelector("div#column2");
+b.onclick = function () {
+  b.classList.toggle("eClicked");
+};
+
+c = document.querySelector("div#column3");
+c.onclick = function () {
+  c.classList.toggle("dClicked");
+};
+
+/* Last prompt then done */
+
+
+ h = document.getElementById("headache");
+h.innerHTML = "I am not going to procastinate anymore.";
+
+
+
